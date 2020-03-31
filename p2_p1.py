cadd3f76da7de83053c1258ba975a72d8836913e
patelddevan <40066975+patelddevan@users.noreply.github.com>
2020-03-30 20:28:59 -0400

    Update p2_p1.py
    Update

--- p2_p1.py
+++ p2_p1.py
@@ -2,15 +2,15 @@ import random
 
 class Graph:
     def __init__(self):
-        self.adjList = dict()
+        self.adjList = {}
 
     def addNode(self, nodeVal):
-        self.adjList[nodeVal] = set()
+        self.adjList[nodeVal] = []
     
     def addUndirectedEdge(self, first, second):
-        self.adjList[first].add(second)
+        self.adjList[first].append(second)
         if first != second:
-            self.adjList[second].add(first)
+            self.adjList[second].append(first)
     
     def removeUndirectedEdge(self, first, second):
         self.adjList[first].remove(second)
@@ -20,19 +20,18 @@ class Graph:
     def getAllNodes(self):
         return set(self.adjList)
 
-    def addDirectedEdge(self, first, second):
-        self.adjList[first].add(second)
-
 class Main:
     @staticmethod
     def createRandomUnweightedGraphIter(n):
         graph = Graph()
         for i in range(n):
             graph.addNode(i)
+        visited = set()
         for node in graph.adjList:
-            for _node in graph.adjList:
-                if bool(random.getrandbits(1)):
+            for _node in graph.adjList: 
+                if _node not in visited and bool(random.getrandbits(1)):
                     graph.addUndirectedEdge(node, _node)
+            visited.add(node)
         return graph
 
     @staticmethod
@@ -43,21 +42,124 @@ class Main:
         graph.addNode(0)
         for i in range(1, n):
             graph.addNode(i)
-            graph.addDirectedEdge(i - 1, i)
+            graph.addUndirectedEdge(i - 1, i)
         return graph
+
+    @staticmethod
+    def BFTRecLinkedList():
+        graph = Main.createLinkedList(100)
+        return GraphSearch.BFTRec(graph)
+    
+    @staticmethod
+    def BFTIterLinkedList():
+        graph = Main.createLinkedList(10000)
+        return GraphSearch.BFTIter(graph)
 
 class GraphSearch:
     @staticmethod
-    def DFSRec(start, end):
-        pass
+    def DFSRec(graph, start, end):
+        arr = []
+        visited = set()
+        return GraphSearch.DFSRecHelper(graph, start, end, arr, visited)
 
+    @staticmethod
+    def DFSRecHelper(graph, start, end, arr, visited):
+        arr.append(start)
+        visited.add(start)
+        if start == end:
+            return 1
+        neighbors = graph.adjList[start]
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                result = GraphSearch.DFSRecHelper(graph, neighbor, end, arr, visited)
+                if result is not None:
+                    return arr
+        return None
 
-def main():
+    @staticmethod
+    def DFSIter(graph, start, end):
+        arr = []
+        visited = set()
+        stack = [start]
+        while len(stack) != 0:
+            next = stack.pop()
+            arr.append(next)
+            visited.add(next)
+            if next == end:
+                return arr
+            neighbors = graph.adjList[next]
+            for neighbor in reversed(neighbors):
+                if neighbor not in visited:
+                    stack.append(neighbor)
+
+    @staticmethod
+    def BFTRec(graph):
+        arr = []
+        visited = set()
+        for node in graph.adjList:
+            if node not in visited:
+                GraphSearch.BFTRecHelper(graph, [node], arr, visited)
+        return arr
+
+    @staticmethod
+    def BFTRecHelper(graph, queue, arr, visited):
+        if len(queue) != 0:
+            next = queue.pop(0)
+            arr.append(next)
+            visited.add(next)
+            neighbors = graph.adjList[next]
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+            return GraphSearch.BFTRecHelper(graph, queue, arr, visited)
+
+    @staticmethod
+    def BFTIter(graph):
+        arr = []
+        visited = set()
+        for node in graph.adjList:
+            if node not in visited:
+                GraphSearch.BFTIterHelper(graph, [node], arr, visited)
+        return arr
+
+    @staticmethod
+    def BFTIterHelper(graph, queue, arr, visited):
+        while len(queue) != 0:
+            next = queue.pop(0)
+            arr.append(next)
+            visited.add(next)
+            neighbors = graph.adjList[next]
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+def main(): 
     graph = Main.createRandomUnweightedGraphIter(10)
-    nodes = graph.getAllNodes()
-    for n in nodes:
-        print(n)
+    dfs_rec_arr = GraphSearch.DFSRec(graph, 3, 9)
+    dfs_iter_arr = GraphSearch.DFSIter(graph, 3, 9)
+    bft_rec_arr = GraphSearch.BFTRec(graph)
+    bft_iter_arr = GraphSearch.BFTIter(graph)
+    print("--- Random Unweighted Graph ---")
+    print("DFS-Rec ", dfs_rec_arr)
+    print("DFS-Iter", dfs_iter_arr)
+    print("BFT-Rec ", bft_rec_arr)
+    print("BFT-Iter", bft_iter_arr)
 
+    _graph = Main.createLinkedList(10)
+    _dfs_rec_arr = GraphSearch.DFSRec(_graph, 3, 9)
+    _dfs_iter_arr = GraphSearch.DFSIter(_graph, 3, 9)
+    _bft_rec_arr = GraphSearch.BFTRec(_graph)
+    _bft_iter_arr = GraphSearch.BFTIter(_graph)
+    print("--- Linked List ---")
+    print("DFS-Rec ", _dfs_rec_arr)
+    print("DFS-Iter", _dfs_iter_arr)
+    print("BFT-Rec ", _bft_rec_arr)
+    print("BFT-Iter", _bft_iter_arr)
+
+    print("--- BFT-Iter Linked List ---")
+    print(Main.BFTIterLinkedList())
+    print("--- BFT-Rec Linked List ---")
+    print(Main.BFTRecLinkedList())
 
 if __name__ == "__main__":
     main()
