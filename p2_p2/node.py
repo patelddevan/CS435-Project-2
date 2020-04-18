276f730b1ecff16b8df4d0668e99b9d521379d1b
Devan Patel <40066975+patelddevan@users.noreply.github.com>
2020-04-18 11:12:05 -0400

    Update node.py
    "Unless I'm confused you are typing the Node as an int in this line. Node shouldn't have an expected type. If you meant that the dictionary's values' expected type should be an int I believe the syntax is Dict[Node, int]." - AnthonyTudorov

--- p2_p2/node.py
+++ p2_p2/node.py
@@ -4,5 +4,5 @@ from typing import Dict, List
 class Node:
     def __init__(self, nodeVal):
         self.nodeVal: int = nodeVal
-        self.neighbors: Dict[Node: int]  = {}
+        self.neighbors: Dict[Node, int]  = {}
         self.coordinate: List[int] = []
